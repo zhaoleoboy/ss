7bb19d83428e6b97f1900b76b2bd6382edf726ba
leo <zhaoleoboy@163.com>
2019-09-10 21:13:21 +0800

    run it

--- consumer/src/main/java/com/ying/demo/controller/UserController.java
+++ consumer/src/main/java/com/ying/demo/controller/UserController.java
@@ -3,20 +3,21 @@ package com.ying.demo.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.ying.demo.api.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-
     @Reference(version = "${user.service.version}")
     private UserService userService;
 
-    public String sayHi(){
+    @GetMapping("/sayHi")
+    public String sayHi() {
         return userService.sayHi();
     }
 
-    public String sayHiError(){
+    public String sayHiError() {
         return "Hystrix Fallback.";
     }
 
